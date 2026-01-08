e3caab6e9c1feec83decd72754666256033be179
Prateek234775 <prateekkumar5470@gmail.com>
2026-01-08 17:03:18 +0530

    Day 4 – Loops in Python

---
+++ Day4_Loops/README.md
@@ -0,0 +1,25 @@
+# Day 4 - Loops in python
+
+## for loop
+Used when number of iterations is known.
+
+Syntax:
+for in range(start, end):
+    statements
+
+## while loop
+Runs until condition becomes false.
+
+Syntax:
+while condition:
+    statements
+
+## break
+Stops the loop immediately.
+
+## continue
+Skips current iteration.
+
+## Key Points
+-Avoid infinite loops 
+-Indentation is mandatory 
---
+++ Day4_Loops/day4.py
@@ -0,0 +1,27 @@
+#for loop python
+
+for i in range(1, 6):
+    print(i)
+    
+#loop through string
+name= "prateek"
+for ch in name:
+    print(ch)
+    
+#while loop example
+count = 1
+while count <=5:
+    print(count)
+    count += 1
+    
+#break example
+for i in range(1, 10):
+    if i ==5:
+        break
+    print(i)
+    
+#continue example
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
